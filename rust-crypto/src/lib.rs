c420477bfe5519da15852a5d586e96235d1f8da7
Maxwell Plotkin <plotkinmaxwell@gmail.com>
2018-11-22 13:11:12 -0800

    some progress on s1c3

--- rust-crypto/src/lib.rs
+++ rust-crypto/src/lib.rs
@@ -3,22 +3,22 @@ mod tests {
     use hex;
     use base64;
 
-    static S1C1_IN: &str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    static S1C1_OUT: &str = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-
-    static S1C2_IN_1: &str = "1c0111001f010100061a024b53535009181c";
-    static S1C2_IN_2: &str = "686974207468652062756c6c277320657965";
-    static S1C2_OUT: &str = "746865206b696420646f6e277420706c6179";
-
     #[test]
     fn s1c1() {
-        assert_eq!(base64::encode(&hex::decode(&String::from(S1C1_IN)).unwrap()), S1C1_OUT);
+        const IN: &str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+        const OUT: &str = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
+        assert_eq!(base64::encode(&hex::decode(IN).unwrap()), OUT);
     }
 
     #[test]
     fn s1c2() {
-        assert_eq!(hex::xor(&hex::decode(S1C2_IN_1).unwrap(), &hex::decode(S1C2_IN_2).unwrap()),
-                   hex::decode(S1C2_OUT).unwrap());
+        const IN_1: &str = "1c0111001f010100061a024b53535009181c";
+        const IN_2: &str = "686974207468652062756c6c277320657965";
+        const OUT: &str = "746865206b696420646f6e277420706c6179";
+
+        assert_eq!(hex::xor(&hex::decode(IN_1).unwrap(), &hex::decode(IN_2).unwrap()),
+                   hex::decode(OUT).unwrap());
     }
 }
 
@@ -27,6 +27,11 @@ pub mod hex {
     static TABLE: [char; 16] = [
         '0', '1', '2', '3', '4', '5', '6', '7',
         '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+    ];
+
+    static COMMON: [u8; 12] = [
+        'E' as u8, 'T' as u8, 'A' as u8, 'O' as u8, 'I' as u8, 'N' as u8,
+        'S' as u8, 'H' as u8, 'R' as u8, 'D' as u8, 'L' as u8, 'U' as u8,
     ];
 
     pub fn decode(mut s: &str) -> Result<Vec<u8>, std::num::ParseIntError> {
@@ -61,6 +66,28 @@ pub mod hex {
         }
 
         out
+    }
+
+    pub fn decipher(bytes: &[u8], key: u8) -> Vec<u8> {
+        let mut out = vec![];
+
+        for byte in bytes {
+            out.push(byte ^ key);
+        }
+
+        out
+    }
+
+    pub fn score_str(s: &str) -> usize {
+        let mut score = 0;
+
+        for c in s.bytes() {
+            if COMMON.contains(&c) {
+                score = score + 1;
+            }
+        }
+
+        score
     }
 }
 
