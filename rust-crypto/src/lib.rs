b1612c3f4289e5578bd36333ae43f8d6a67af73d
Maxwell Plotkin <maxwell.a.plotkin@jpl.nasa.gov>
2018-11-23 14:33:54 -0800

    finished set 1 challenge 3

--- rust-crypto/src/lib.rs
+++ rust-crypto/src/lib.rs
@@ -20,6 +20,18 @@ mod tests {
         assert_eq!(hex::xor(&hex::decode(IN_1).unwrap(), &hex::decode(IN_2).unwrap()),
                    hex::decode(OUT).unwrap());
     }
+
+    #[test]
+    fn s1c3() {
+        const IN: &str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+        const KEY: u8 = 88;
+        const OUT: &str = "Cooking MC's like a pound of bacon";
+
+        println!("{}", hex::find_key(IN));
+
+        assert_eq!(hex::find_key(IN), KEY);
+        assert_eq!(std::str::from_utf8(&hex::decipher(&hex::decode(IN).unwrap(), KEY)).unwrap(), OUT);
+    }
 }
 
 pub mod hex {
@@ -29,9 +41,14 @@ pub mod hex {
         '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
     ];
 
-    static COMMON: [u8; 12] = [
+    static COMMON_UPPER: [u8; 12] = [
         'E' as u8, 'T' as u8, 'A' as u8, 'O' as u8, 'I' as u8, 'N' as u8,
         'S' as u8, 'H' as u8, 'R' as u8, 'D' as u8, 'L' as u8, 'U' as u8,
+    ];
+
+    static COMMON_LOWER: [u8; 12] = [
+        'e' as u8, 't' as u8, 'a' as u8, 'o' as u8, 'i' as u8, 'n' as u8,
+        's' as u8, 'h' as u8, 'r' as u8, 'd' as u8, 'l' as u8, 'u' as u8,
     ];
 
     pub fn decode(mut s: &str) -> Result<Vec<u8>, std::num::ParseIntError> {
@@ -82,12 +99,38 @@ pub mod hex {
         let mut score = 0;
 
         for c in s.bytes() {
-            if COMMON.contains(&c) {
+            if COMMON_UPPER.contains(&c) || c == ' ' as u8 {
                 score = score + 1;
+            } else if COMMON_LOWER.contains(&c) {
+                score = score + 2;
             }
         }
 
         score
+    }
+
+    pub fn find_key(s: &str) -> u8 {
+        let in_bytes: Vec<u8> = decode(s).unwrap();
+        let mut max_score = 0;
+        let mut out = 0;
+
+        for key in 1..128 {
+            let d = decipher(&in_bytes, key as u8);
+
+            let s = match std::str::from_utf8(&d) {
+                Ok(v) => v,
+                Err(_) => ""
+            };
+
+            let score = score_str(&s);
+
+            if score > max_score {
+                out = key;
+                max_score = score;
+            }
+        }
+
+        out
     }
 }
 
